9c4db7ff86b8e3ed2ef35a05f19b1a3e875503a8
ZhangZisu <admin@zhangzisu.cn>
2019-07-12 14:24:53 +0800

    Add shared schemas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
   const server = require('fastify')({ logger: true })
   await server
     .register(require('fastify-sensible'))
+    .register(require('./schemas/shared'))
     .register(require('./db'))
     .register(require('./routes'))
     .listen(3000)
--- schemas/shared/index.js
+++ schemas/shared/index.js
@@ -4,6 +4,7 @@ const wrapper = require('fastify-plugin')
 
 module.exports = wrapper(async (server, opts) => {
   fs.readdirSync(__dirname).filter(x => x.endsWith('.json')).forEach(name => {
+    console.log(`Loading shared schema: ${name}`)
     server.addSchema(require(path.join(__dirname, name)))
   })
 })
